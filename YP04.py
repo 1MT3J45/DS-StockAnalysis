606f2b82cedb8e1fcbfc3276a8449f2a99bcfcd2
1mt3j45 <agenttj.rawal98@gmail.com>
2018-05-27 19:15:22 +0530

    Manual Input for Stock

--- YP04.py
+++ YP04.py
@@ -11,8 +11,10 @@ import xlsxwriter
 import os
 import glob
 
+
 def excel_file_filter(filename, extensions=['.xls', '.xlsx']):
     return any(filename.endswith(e) for e in extensions)
+
 
 def get_filenames(root):
     filename_list = []
@@ -21,25 +23,28 @@ def get_filenames(root):
             filename_list.append(os.path.join(path, filename))
     return filename_list
 
+
 def common_elements(list1, list2):
     result = []
     for element in list1:
         if element in list2:
             result.append(list(set(list1) | set(list2)))
     return result
+
+
 filenames = get_filenames('Grouping_on_Following_Day')
 
 ofxls = []  # open file type xls 
 for i in range(len(filenames)):
-    ofxls.append('')
-    ofxls[i] = xlrd.open_workbook(filenames[i])
+    ofxls.append(xlrd.open_workbook(filenames[i]))
 
-StockLists =[]
-CountLists = []     
+StockLists = []
+CountLists = []
 SheetCount = []
-for i in range(0, len(filenames)):   # Iteration over each data file
+Input = raw_input('Enter Stock Name:')
+for i in range(0, len(filenames)):  # Iteration over each data file
     DayListCount = []
-    DayStockList = []    
+    DayStockList = []
     StockList = []
     Day = ofxls[i]
     DaySheetCount = Day.nsheets
@@ -47,18 +52,22 @@ for i in range(0, len(filenames)):   # Iteration over each data file
     for j in range(DaySheetCount):  #
         Cluster = Day.sheet_by_index(j)
         StockList = []
-        for k in range(0, Cluster.nrows):   #((1, Cluster.nrows-3)
-            StockList.append(str(Cluster.cell(k,0).value))
+        for k in range(0, Cluster.nrows):  # ((1, Cluster.nrows-3)
+            if k == 3:
+                StockList.append(Input.upper())
+            else:
+                StockList.append(str(Cluster.cell(k, 0).value))
+
         print('ClusterName', DayClusterNames[j], 'StockList length', len(StockList))
         DayStockList.append(StockList)
-    DayListCount.append(Cluster.nrows-4)
+    DayListCount.append(Cluster.nrows - 4)
     StockLists.append(DayStockList)
     SheetCount.append(DaySheetCount)
-	
+
 RefStockList = StockLists[0][0]
 RefLen = len(RefStockList)
-print('Reference list length',RefLen)
-print('Reference Stock list',RefStockList)
+print('Reference list length', RefLen)
+print('Reference Stock list', RefStockList)
 
 for i in range(1, len(StockLists)):
     ResultList = []
@@ -66,10 +75,9 @@ for i in range(1, len(StockLists)):
     ResultLists = []
     for j in range(len(StockLists[i])):
         ResultList = common_elements(RefStockList, StockLists[i][j])
-        #ResultLists.append(ResultList)
+        # ResultLists.append(ResultList)
         # length.append(len(ResultList))
-    	
-			
+
     # print(length)
     # Max = max(length)
     # print('Max', Max)
@@ -78,6 +86,5 @@ for i in range(1, len(StockLists)):
     # RefStockList = ResultLists[Idx]
     # RefLen = len(RefStockList)
     # print('Reference list length', RefLen, 'for loop Number', i)
-    print('Reference Stock list',ResultList)
-    #Write output to file
-   
+    print 'Reference Stock list', ResultList
+    # Write output to file
