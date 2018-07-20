Cross Platform Issues Fixed & Stock OHLC Averages now available

hourlyData.py
@@ -6,6 +6,7 @@ import csv
 import datetime
 import re
 import pandas as pd
+import numpy as np
 
 import requests
 
@@ -60,17 +61,36 @@ def write_csv(self, filename):
 
 TCKR = pd.read_csv('TICKERS_testing.csv')
 df_list = list()
-df_avg_list = list()
+df_means = pd.DataFrame()
+
 for i in range(TCKR.__len__()):
     print("%s".center(40, '_')%TCKR.iloc[i, 0])
-    df_list.append(get_google_finance_intraday(TCKR.iloc[i,0], 3600, 1))
+    df_list.append(get_google_finance_intraday(TCKR.iloc[i, 0], 3600, 1))
     if not df_list[i].empty:
+        df_avg_list = list()
+
         dim = df_list[i].shape
         df_list[i]['Ticker'] = pd.Series([TCKR.iloc[i, 0]]*dim[0]).values.reshape(-1, 1)
+        temp = np.array(df_list[i])
+        df_tckr = df_list[i]
+        for j in range(0, dim[1]+1):
+            avrg = 0
+            if j < dim[1]:
+                # Numeric Columns: OHLC
+                avrg = temp[:, j].mean()
+                df_avg_list.append(avrg)
+            elif j == dim[1]:
+                # String Column: Ticker Name
+                df_avg_list.append(TCKR.iloc[i, 0]+'__AVG')
+
+        df_list[i] = df_tckr.append(dict(zip(df_tckr.columns, df_avg_list)), ignore_index=True)
+        df_means = df_means.append(df_list[i].iloc[-1, :])
+
         print(df_list[i])
     else:
         print 'No Stocks recorded for', TCKR.iloc[i, 0]
 # TODO Get avg of OHLC & Store in CSV
 StockConclave = pd.concat(df_list)
 df = StockConclave.values
-StockConclave.to_csv('Results/StockConclave.csv')
+StockConclave.to_csv('Results/StockConclave.csv', index=False)
+df_means.to_csv('Results/StockAverages.csv', index=False)